050b52e697686931c44e21a9dcce0f932cd646b4
npg2108 <npg2108@cumc.columbia.edu>
2018-08-13 09:36:00 -0400

    Establishing new version name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cohorts',
-      version='0.1a',
+      version='0.2a',
       description='Proteomics exosome_pgf cohort structure',
       author='Tatonetti Lab',
       author_email='npg2108@cumc.columbia.edu',
