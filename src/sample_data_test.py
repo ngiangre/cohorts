4a369c12f7699cbee2f5ba51abe079d4ef3364f1
npg2108 <npg2108@cumc.columbia.edu>
2018-08-13 10:29:11 -0400

    Upload sample data with cohorts v0.2

--- src/sample_data_test.py
+++ src/sample_data_test.py
@@ -1,16 +1,9 @@
 """
 Testing cohorts package using sample data
 """
-# Setting working directory
-import os
-os.chdir('/Users/npg2108/Github/cohorts/src/')
-
-#Importing modules
-import sys
-sys.path.append("../cohorts")
-from utils import *
-from cohort import *
-from helpers import *
+import pkg_resources
+pkg_resources.require("cohorts==0.2a0")
+import cohorts
 
 #setting input parameters to make cohorts
 top = "~/Github/cohorts/"
@@ -18,17 +11,19 @@ file_dirs ={ 'sample' : top+"sample_data/"}
 replicates_files = { 'sample' : "df_replicates.tsv"}
 sample_groups_files = { 'sample' : "df_sample_groups.tsv"}
 name = 'sample'
-obj = Cohort(cohort = name,
+obj = cohorts.Cohort(cohort = name,
                         file_dir=file_dirs[name],
                         replicates_file=replicates_files[name],
                         sample_groups_file=sample_groups_files[name])
 
 treats = { 'sample' : 'trt' }
 refs = {'sample' : 'ref' }
-objs = make_cohorts_dict(names=[name],
+objs = cohorts.cohort.make_cohorts_dict(names=[name],
                         file_dirs=file_dirs,
                         replicates_files=replicates_files,
                         sample_groups_files=sample_groups_files,
                         treats=treats,refs=refs)
+
+objs
 
 print(obj.__doc__)
