56bb863276b51381236927ead1b78d7764030db8
npg2108 <npg2108@cumc.columbia.edu>
2018-08-13 10:18:26 -0400

    mod

--- cohorts/cohort.py
+++ cohorts/cohort.py
@@ -1,5 +1,4 @@
 from .utils import *
-from .helpers import *
 
 import os
 import numpy as np
@@ -1307,6 +1306,40 @@ class IntegratedCohort(object):
 		self.df = multi_df_join(dfs).dropna()
 		self.df_groups = multi_df_join(df_groups).dropna()
 
+def make_cohorts_dict(names,
+					file_dirs,
+					treats,refs,
+					replicates_files,
+					sample_groups_files,
+					data_dir="../../data/",
+					uniprot_file="uniprot-all_20171124.tab.gz"):
+	"""
+	Create cohort objects via dictionaries of attributes where the key of each dfictionary is the name of the cohort object
+	
+	Parameters:
+	----------
+
+
+	Returns:
+	------
+
+	objs
+		Dictionary where keys are each cohort object name and values are the instantiaated cohort object.
+	"""
+	if len(names)==0:
+		return {}
+	else:
+		objs = {}
+		for cohort in names:
+			obj = Cohort(cohort=cohort,
+								data_dir=data_dir,file_dir=file_dirs[cohort],
+								replicates_file=replicates_files[cohort],
+								sample_groups_file=sample_groups_files[cohort],
+								uniprot_file=uniprot_file)
+			obj.set_ref(refs[cohort])
+			obj.set_treat(treats[cohort])
+			objs[cohort] = obj
+		return objs
 
 if __name__=="__main__":
 	"""
