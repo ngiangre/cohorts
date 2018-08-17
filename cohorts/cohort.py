b6d0799a4c2847925598ca4cfb4faf5c9c7cf289
npg2108 <npg2108@cumc.columbia.edu>
2018-08-17 12:29:16 -0400

    tidy data function of cohort attribute

--- cohorts/cohort.py
+++ cohorts/cohort.py
@@ -739,6 +739,70 @@ class Cohort(object):
 
 		return tab
 
+	def get_tidy_data(attribute=""):
+	"""
+	Returns tidy version of available attributes
+	
+	Parameters
+	----------
+	
+	attribute: str
+		one of ['sample_groups', 'replicate_groups', 'raw_replicates', 'raw_samples','replicates_hq','samples_hq']
+	
+	Output:
+	-------
+	
+	tidy version of attribute
+	
+	"""
+	
+	avail_attributes = ['sample_groups', 'replicate_groups', 'raw_replicates', 'raw_samples','replicates_hq','samples_hq']
+
+	if attribute not in avail_attributes:
+		raise Exception("Not one of:\n{}".format(avail_attributes))
+	elif attribute == 'sample_groups':
+		tidy = (getattr(obj,attribute)
+			.reset_index()
+			.rename(columns={ 'index' : 'Group' })
+			.melt(id_vars='Group',var_name='Sample')
+			.query('value==1')
+			.drop(['value'],axis=1)
+			)
+	elif attribute == 'replicate_groups':
+		tidy = (getattr(obj,attribute)
+			.reset_index()
+			.rename(columns={ 'index' : 'Group' })
+			.melt(id_vars='Group',var_name='Replicate')
+			.query('value==1')
+			.drop(['value'],axis=1)
+			)
+	elif attribute == 'raw_replicates':
+		tidy = (getattr(obj,attribute)
+			.reset_index()
+			.rename(columns={ 'index' : 'Protein' })
+			.melt(id_vars='Protein',var_name='Replicate',value_name='Value')
+			)
+	elif attribute == 'replicates_hq' and getattr(obj,attribute) is not None:
+		tidy = (getattr(obj,attribute)
+			.reset_index()
+			.rename(columns={ 'index' : 'Protein' })
+			.melt(id_vars='Protein',var_name='Replicate',value_name='Value')
+			)
+	elif attribute == 'raw_samples':
+		tidy = (getattr(obj,attribute)
+			.reset_index()
+			.rename(columns={ 'index' : 'Protein' })
+			.melt(id_vars='Protein',var_name='Sample',value_name='Value')
+			)
+	elif attribute == 'samples_hq' and getattr(obj,attribute) is not None:
+		tidy = (getattr(obj,attribute)
+			.reset_index()
+			.rename(columns={ 'index' : 'Protein' })
+			.melt(id_vars='Protein',var_name='Sample',value_name='Value')
+			)
+	return tidy
+
+
 	#ANALYSIS FUNCTIONS
 
 	def manual_feature_extraction(self,df):
@@ -990,9 +1054,9 @@ class Cohort(object):
 
 		#get indices of replicates/samples in groups
 		inds = np.where(df_groups.loc[grp] == 1)
-        
-        #get 
-        
+		
+		#get 
+		
 		return df.T.iloc[inds].T
 
 	#PROTEIN SUBSET FUNCTIONS
