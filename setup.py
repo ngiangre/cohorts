be373d410f596fc030fa93e4c732c1c3faf80a5a
Nick Giangreco <npg2108@cumc.columbia.edu>
2018-12-06 10:33:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='cohorts',
       version='0.2a',
       description='Proteomics exosome_pgf cohort structure',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/pypa/sampleproject',
       author='Tatonetti Lab',
       author_email='npg2108@cumc.columbia.edu',
+      classifiers=[
+	      'Development Status :: 3 - Alpha',
+	      'Programming Language :: Python :: 3'
+      ],
+      keywords='clinical proteomics data management tool',
       license='MIT',
-      packages=['cohorts'],
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
       install_requires=[
       		'os',
       		'numpy',
