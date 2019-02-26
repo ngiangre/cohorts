3b704ea16bc667e301f2f93bce8de7db2120a3d4
npg2108 <npg2108@cumc.columbia.edu>
2019-02-25 20:28:19 -0500

    Updated license and setup

--- setup.py
+++ setup.py
@@ -1,36 +1,35 @@
-from setuptools import setup, find_packages
+import setuptools
 from io import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md','r') as f:
     long_description = f.read()
 
 
-setup(name='cohorts',
-      version='0.3a',
-      description='Proteomics exosome_pgf cohort structure',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/ngiangre/cohorts',
-      author='Nicholas Giangreco',
-      author_email='npg2108@cumc.columbia.edu',
-      classifiers=[
-	      'Development Status :: 3 - Alpha',
-	      'Programming Language :: Python :: 3'
-      ],
-      keywords='clinical proteomics data management tool',
-      license='MIT',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      install_requires=[
-      		'numpy',
-      		'pandas',
-      		'logging',
-      		'scipy',
-      		'scikit-learn'
-      	],
-		test_suite='nose.collector',
-		tests_require=['nose'],
-      zip_safe=False)
+setuptools.setup(
+	name='cohorts',
+	version='0.3a',
+	description='Proteomics exosome_pgf cohort structure',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/ngiangre/cohorts',
+	author='Nicholas Giangreco',
+	author_email='npg2108@cumc.columbia.edu',
+	classifiers=[
+	'Development Status :: 3 - Alpha',
+	'Programming Language :: Python :: 3'
+	],
+	keywords='clinical proteomics data management tool',
+	license='MIT',
+	packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests']),
+	install_requires=[
+	'numpy',
+	'pandas',
+	'logging',
+	'scipy',
+	'scikit-learn'
+	],
+	test_suite='nose.collector',
+	tests_require=['nose'],
+	zip_safe=False)
