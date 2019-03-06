9c07514e35b5b022545ef9ea5550564f96883a7a
npg2108 <npg2108@cumc.columbia.edu>
2019-03-06 17:36:24 -0500

    added cohort object test

--- tests/test_cohort.py
+++ tests/test_cohort.py
@@ -1,32 +1,35 @@
-'''
+import unittest
 from cohorts import Cohort
 
-top = "../"
-data_dirs = { 'my_cohort' : top+"data/" }
-file_dirs = { 'my_cohort' : top+"sample_data/" }
-replicates_files = { 'my_cohort' : "df_replicates.tsv" }
-#sample_files = { 'my_cohort' : "df_samples.tsv" }
-sample_groups_files = { 'my_cohort' : "df_sample_groups.tsv" }
-cohort = { 'my_cohort' : 'my_cohort' }
-references = { 'my_cohort' : 'ref' }
-treatments = { 'my_cohort' : 'trt' }
-cohort_name = 'my_cohort'
-c = Cohort(cohort=cohort[cohort_name],marker_type="protein",
-					data_dir=data_dirs[cohort_name],file_dir=file_dirs[cohort_name],
-					replicates_file=replicates_files[cohort_name],
-					#sample_files=sample_files[cohort],
-					sample_groups_file=sample_groups_files[cohort_name],
-					reference=references[cohort_name],
-					treatment=treatments[cohort_name])
+class TestStringMethods(object):
 
-def test():
 
-	assert isinstance(c.cohort,str)
+	def __init__(self):
 
-'''
+		top = "./"
+		data_dirs = { 'my_cohort' : top+"data/" }
+		file_dirs = { 'my_cohort' : top+"sample_data/" }
+		replicates_files = { 'my_cohort' : "df_replicates.tsv" }
+		#sample_files = { 'my_cohort' : "df_samples.tsv" }
+		sample_groups_files = { 'my_cohort' : "df_sample_groups.tsv" }
+		cohort = { 'my_cohort' : 'my_cohort' }
+		references = { 'my_cohort' : 'ref' }
+		treatments = { 'my_cohort' : 'trt' }
+		cohort_name = 'my_cohort'
+		c = Cohort(cohort=cohort[cohort_name],marker_type="protein",
+							data_dir=data_dirs[cohort_name],file_dir=file_dirs[cohort_name],
+							replicates_file=replicates_files[cohort_name],
+							#sample_files=sample_files[cohort],
+							sample_groups_file=sample_groups_files[cohort_name],
+							reference=references[cohort_name],
+							treatment=treatments[cohort_name])
 
-# given 
-def test():
-	
-	return True
+		self.cohort = c
+
+	def test_cohort_name(self):
+		return self.cohort.cohort=='my_cohort'
+
+if __name__ == '__main__':
+	unittest.main()
+
 
